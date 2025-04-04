4ce9221610b5971b1acad8c835e191683a6f28f8
FlameSoulis <flamesoulis@flamesoulis.com>
2025-04-04 18:18:18 -0400

    Refactor main.py to use classes to avoid global chaos

--- main.py
+++ main.py
@@ -5,48 +5,46 @@ import pygame
 from constants import *
 from player import *
 
-pygame_clock = None
-pygame_screen = None
-pygame_updatables = None
-pygame_drawables = None
-dt = 0
-player = None
+class Asteroids:
 
-def mainloop():
-	global pygame_screen, pygame_clock, dt
-	global pygame_updatables, pygame_drawables
-	global player
-	while True:
-		for event in pygame.event.get():
-		    if event.type == pygame.QUIT:
-		        return
-		# updates here
-		pygame_updatables.update(dt)
-		# rendering nonsense beyond this point
-		pygame_screen.fill("black")
-		for drawable in pygame_drawables:
-			drawable.draw(pygame_screen)
-		pygame.display.flip()
-		dt = pygame_clock.tick(60) / 1000
+	def __init__(self):
+		self.clock = None
+		self.screen = None
+		self.updatables = None
+		self.drawables = None
+		self.dt = 0
+		self.player = None
 
-def main():
-	global pygame_screen, pygame_clock
-	global pygame_updatables, pygame_drawables
-	global player
-	print("Starting Asteroids!")
-	pygame.init()
-	pygame_clock = pygame.time.Clock()
-	pygame_updatables = pygame.sprite.Group()
-	pygame_drawables = pygame.sprite.Group()
-	Player.containers = (pygame_updatables, pygame_drawables)
-	player = Player(
-		SCREEN_WIDTH / 2,
-		SCREEN_HEIGHT / 2
-	)
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
-	pygame_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	mainloop()
+	def mainloop(self):
+		while True:
+			for event in pygame.event.get():
+			    if event.type == pygame.QUIT:
+			        return
+			# updates here
+			self.updatables.update(self.dt)
+			# rendering nonsense beyond this point
+			self.screen.fill("black")
+			for drawable in self.drawables:
+				drawable.draw(self.screen)
+			pygame.display.flip()
+			self.dt = self.clock.tick(60) / 1000
+
+	def main(self):
+		print("Starting Asteroids!")
+		pygame.init()
+		self.clock = pygame.time.Clock()
+		self.updatables = pygame.sprite.Group()
+		self.drawables = pygame.sprite.Group()
+		Player.containers = (self.updatables, self.drawables)
+		self.player = Player(
+			SCREEN_WIDTH / 2,
+			SCREEN_HEIGHT / 2
+		)
+		print(f"Screen width: {SCREEN_WIDTH}")
+		print(f"Screen height: {SCREEN_HEIGHT}")
+		self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+		self.mainloop()
 
 if __name__ == "__main__":
-    main()
+    game = Asteroids()
+    game.main()
