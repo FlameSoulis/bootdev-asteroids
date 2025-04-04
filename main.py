57026b2b99564f076becfdb71a11b968d0941622
FlameSoulis <flamesoulis@flamesoulis.com>
2025-04-03 20:22:43 -0400

    Added player object and drawing them

--- main.py
+++ main.py
@@ -3,22 +3,38 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
-def mainloop(screen):
+pygame_clock = None
+pygame_screen = None
+dt = 0
+player = Player(
+	SCREEN_WIDTH / 2,
+	SCREEN_HEIGHT / 2
+)
+
+def mainloop():
+	global pygame_screen, pygame_clock, dt
+	global player
 	while True:
 		for event in pygame.event.get():
 		    if event.type == pygame.QUIT:
 		        return
-		screen.fill("black")
+		pygame_screen.fill("black")
+		player.draw(pygame_screen)
 		pygame.display.flip()
+		dt = pygame_clock.tick(60) / 1000
 
 def main():
+	global pygame_screen, pygame_clock
+	global player
 	print("Starting Asteroids!")
 	pygame.init()
+	pygame_clock = pygame.time.Clock()
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	mainloop(screen)
+	pygame_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	mainloop()
 
 if __name__ == "__main__":
     main()
